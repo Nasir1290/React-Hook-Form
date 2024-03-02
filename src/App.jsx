61eb3685fc29d27ad364107020feddf58509a2a7
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-02 21:24:02 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div>
       <h1 className=' text-2xl'>Hello</h1>
+      <input className=' input' type="text" placeholder='Enter something...' />
     </div>
   )
 }
